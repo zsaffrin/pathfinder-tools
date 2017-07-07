c3befaca45ba7b21881d1b0c360b48f53b4384fd
Zach Saffrin <zach@zachsaffrin.com>
2017-07-06 22:53:30 -0400

    Update API route for new filename

--- server/api-routes.js
+++ server/api-routes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = (app) => {
 	app.route('/api/loot')
 		.get((req, res) => {
-			fs.readFile('./server/data/loot-list.json', (err, data) => {
+			fs.readFile('./server/data/loot.json', (err, data) => {
 				if (err) {
 					res.send(err);
 				}
