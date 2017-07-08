0813c4a9a47212143b6de35355a6e0b27fb445a6
Zach Saffrin <zach@zachsaffrin.com>
2017-07-07 20:47:35 -0400

    Adds category splitting

--- src/components/LootList.js
+++ src/components/LootList.js
@@ -1,5 +1,63 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import 'whatwg-fetch';
+
+const LootItemEntry = ({ name, category, bookValue, claimed, claimedBy }) => {
+	let salePrice;
+	if (!claimed) {
+		if (category === 9) {
+			salePrice = bookValue;
+		} else {
+			salePrice = Math.floor(bookValue / 2);
+		}
+	}
+
+	return (
+		<div className="flex">
+			<div
+				className={`${claimed ? 'gray-light italic' : ''}`}
+				style={{ width: '20rem' }}
+			>
+				{name}
+			</div>
+
+			<div
+				className={`${claimed ? 'gray-light italic' : ''}`}
+				style={{ width: '7rem' }}
+			>
+				{bookValue}
+			</div>
+
+			<div
+				className={`${claimed ? 'gray-light italic' : ''}`}
+				style={{ width: '7rem' }}
+			>
+				{salePrice}
+			</div>
+
+			<div
+				className={`${claimed ? 'gray-light italic' : ''}`}
+				style={{ width: '4rem' }}
+			>
+				{claimed ? claimedBy : ''}
+			</div>
+		</div>
+	);
+};
+LootItemEntry.propTypes = {
+	name: PropTypes.string,
+	category: PropTypes.number,
+	bookValue: PropTypes.number,
+	claimed: PropTypes.bool,
+	claimedBy: PropTypes.string,
+};
+LootItemEntry.defaultProps = {
+	name: '',
+	category: 0,
+	bookValue: 0,
+	claimed: false,
+	claimedBy: '',
+};
 
 class LootList extends Component {
 	static fetchLootItemsFromServer() {
@@ -11,20 +69,32 @@ class LootList extends Component {
 		});
 	}
 
+	static filterItemsByCategory(items, category) {
+		const contents = [];
+		items.map((item) => {
+			if (item.category === category) {
+				return contents.push(item);
+			}
+			return false;
+		});
+		return contents;
+	}
+
 	constructor() {
 		super();
 		this.state = {
 			loot: {
 				items: [],
+				categories: [],
 			},
 		};
 	}
 
 	componentDidMount() {
-		this.loadLootItems();
+		this.populateLootItems();
 	}
 
-	loadLootItems() {
+	populateLootItems() {
 		LootList.fetchLootItemsFromServer()
 			.then(loot => this.setState({ loot }));
 	}
@@ -32,49 +102,47 @@ class LootList extends Component {
 	render() {
 		const { loot } = this.state;
 
-		const lootItemNodes = loot.items.map(({
-			claimed, claimedBy, id, name, bookValue,
-		}) => {
-			let claimCell;
-			if (claimed) {
-				claimCell = (
-					<div
-						className="gray-light italic"
-						style={{ width: '10rem' }}
-					>
-						{claimedBy}
-					</div>
-				);
-			}
-
-			return (
-				<div className="flex" key={id}>
-					<div
-						className={`${claimed ? 'gray-light italic' : ''}`}
-						style={{ width: '13rem' }}
-					>
-						{name}
-					</div>
-
-					<div
-						className={`${claimed ? 'gray-light italic' : ''}`}
-						style={{ width: '4rem' }}
-					>
-						{bookValue}
-					</div>
-
-					{claimCell}
-				</div>
-			);
-		});
-
 		return (
 			<div>
 				<h2>Loot List</h2>
 				{loot.items.length} items loaded
-				<div className="my2 p1">
-					{lootItemNodes}
+
+				<div className="flex flex-wrap">
+					{loot.categories.map(({ id, name }) => {
+						const categoryItems = LootList.filterItemsByCategory(loot.items, id);
+
+						if (categoryItems.length > 0) {
+							return (
+								<div className="p1" key={id}>
+									<h3>{name}</h3>
+									<div className="my2 p1">
+										<div className="flex heading border-bottom border-gray-dark">
+											<div style={{ width: '20rem' }}>
+												Item
+											</div>
+											<div style={{ width: '7rem' }}>
+												Book Price
+											</div>
+											<div style={{ width: '7rem' }}>
+												Sale Price
+											</div>
+											<div style={{ width: '4rem' }}>
+												Claim
+											</div>
+										</div>
+
+										{categoryItems.map(item => (
+											<LootItemEntry {...item} key={item.id} />
+										))}
+									</div>
+								</div>
+							);
+						}
+
+						return false;
+					})}
 				</div>
+
 			</div>
 		);
 	}
