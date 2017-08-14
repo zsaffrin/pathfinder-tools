6b8c0b606059510d070e1974a68aba07c4bad71d
Zach Saffrin <zach@zachsaffrin.com>
2017-08-13 22:27:22 -0400

    Move ability mod calc to Character class

--- src/components/_staticFunctions.js
+++ src/components/_staticFunctions.js
@@ -6,9 +6,3 @@ export const showPlus = num => (
 export const formatLength = inches => (
 	`${inches}"`
 );
-
-export const calcAbilityMod = score => (
-	Math.floor((score - 10) / 2)
-);
-
-export default showPlus;
