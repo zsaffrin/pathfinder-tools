68faaf11fda181a1d80c4dcbdd1e54e1af998907
Zach Saffrin <zach@zachsaffrin.com>
2017-08-23 15:18:13 -0400

    Use Game instead of Character for sizeMod. Breakout Profile props instead of passing whole character object. Separate concerns for Ability scores

--- src/components/characterSheet/CharacterSheet.js
+++ src/components/characterSheet/CharacterSheet.js
@@ -6,7 +6,7 @@ import Abilities from './Abilities';
 import Strength from './Strength';
 import Movement from './Movement';
 
-import { flattenAndCleanArray, applyEffects, showPlus } from '../_staticFunctions';
+import { flattenAndCleanArray, applyEffects } from '../_staticFunctions';
 
 const CharacterSheet = ({ character, gameData }) => {
 	// Collect active effects
@@ -18,17 +18,14 @@ const CharacterSheet = ({ character, gameData }) => {
 
 	// Abilities
 	const abilityScores = character.abilityScores();
-	const abilityTableRows = Object.keys(abilityScores).map((key) => {
-		const abilityMod = character.calcAbilityMod(abilityScores[key]);
-
-		return (
-			<tr>
-				<td className="caps">{key}</td>
-				<td>{abilityScores[key]}</td>
-				<td>{showPlus(abilityMod)}</td>
-			</tr>
-		);
-	});
+	const abilityScoresEffects = [];
+	Object.keys(abilityScores).map(key => (
+		abilityScoresEffects.push({
+			ability: key,
+			score: applyEffects(abilityScores[key], key, effects),
+			mod: character.calcAbilityMod(abilityScores[key]),
+		})
+	));
 
 	// Strength
 	const carry = gameData.carryingCapacity(character.strength());
@@ -41,21 +38,34 @@ const CharacterSheet = ({ character, gameData }) => {
 	// Movement
 	const baseSpeed = gameData.baseLandSpeedByRace(character.race());
 	const baseSpeedEffects = applyEffects(baseSpeed, 'baseSpeed', effects);
-	const swimSpeed = gameData.swimSpeed(baseSpeedEffects.total);
-	const climbSpeed = gameData.climbSpeed(baseSpeedEffects.total);
 
 	return (
 		<div id="character-sheet">
-			<Profile character={character} />
+			<Profile
+				name={character.characterName()}
+				hd={character.hd()}
+				classLevelSummary={character.classLevelSummary()}
+				alignment={character.alignment()}
+				race={character.race()}
+				age={character.age()}
+				size={character.size()}
+				sizeMod={gameData.sizeMod(character.size())}
+				height={character.height()}
+				weight={character.weight()}
+				eyes={character.eyeColor()}
+				hair={character.hairColor()}
+				skin={character.skinColor()}
+				languages={character.languages()}
+			/>
 
-			<Abilities abilityTableRows={abilityTableRows} />
+			<Abilities abilityScores={abilityScoresEffects} />
 
 			<Strength carry={carry} lift={lift} />
 
 			<Movement
 				baseSpeed={baseSpeedEffects}
-				swimSpeed={swimSpeed}
-				climbSpeed={climbSpeed}
+				swimSpeed={gameData.swimSpeed(baseSpeedEffects.total)}
+				climbSpeed={gameData.climbSpeed(baseSpeedEffects.total)}
 			/>
 		</div>
 	);
