0c55808c9bdeb2f4c685361d5e356848569e5db3
Zach Saffrin <zach@zachsaffrin.com>
2017-08-23 22:37:43 -0400

    Add top margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-	<footer id="footer" className="bg-red-darker gray-lighter p2">
+	<footer id="footer" className="mt4 bg-red-darker gray-lighter p2">
 		Made by Zach Saffrin
 	</footer>
 );
